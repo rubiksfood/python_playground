bc6accd43b5deb3e390c9bd29bd95a574f54433f
rubiksfood <rubiksfood@gmail.com>
2025-05-22 20:16:09 +0200

    feat- day1, part1: Solved

--- advent_of_code_23/day1/day1.py
+++ advent_of_code_23/day1/day1.py
@@ -1,3 +1,21 @@
 with open("advent_of_code_23/day1/input.txt") as file:
     data = file.read().splitlines()
-   
+    def answer_1():
+        running_total = 0
+        for value in data:
+            digit_one = None
+            digit_two = None
+            for char in value:
+                if char.isdigit():
+                    digit_one = char
+                    break
+            valueReverse = value[::-1]
+            for char in valueReverse:
+                if char.isdigit():
+                    digit_two = char
+                    break
+            combined = digit_one + digit_two
+            combined = int(combined)
+            running_total += combined
+        print(running_total)
+    answer_1()
