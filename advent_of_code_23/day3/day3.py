b33b784a09674675b104bd2c90ed3d2cc6ac1d48
rubiksfood <rubiksfood@gmail.com>
2025-06-21 13:23:56 +0200

    feat- AoC23 day3, part2: Solved

--- advent_of_code_23/day3/day3.py
+++ advent_of_code_23/day3/day3.py
@@ -1,6 +1,6 @@
 import re
 
-with open("advent_of_code_23/day3/test_input.txt") as file:
+with open("advent_of_code_23/day3/input.txt") as file:
     data = file.read().splitlines()
     def answer_1():
         symbols = [',', '!', '?', ':', ';', '-', '_', '=', '+', '*', '/', '\\', '|', '(', ')', '[', ']', '{', '}', '<', '>', '"', "'", '@', '#', '$', '%', '^', '&', '~', '`']
@@ -48,22 +48,54 @@ with open("advent_of_code_23/day3/test_input.txt") as file:
             for x in range(len(data[y])):
                 if (data[y][x] == '*'):
                     num_list = []
+
                     if (x - 1 >= 0 and data[y][x - 1].isdigit()):
                         leftmost = x - 1
                         while (leftmost - 1 >= 0 and data[y][leftmost - 1].isdigit()):
                             leftmost -= 1
+                        if (num_list and num_list[-1] == int(data[y][leftmost:x])):
+                            continue
                         num_list.append(int(data[y][leftmost:x]))
 
-                    if (x + 1 >= 0 and data[y][x + 1].isdigit()):
+                    if (x + 1 < len(data[y]) and data[y][x + 1].isdigit()):
                         rightmost = x + 1
                         while (rightmost + 1 < len(data[y]) and data[y][rightmost + 1].isdigit()):
                             rightmost += 1
+                        if (num_list and num_list[-1] == int(data[y][x + 1:rightmost + 1])):
+                            continue
                         num_list.append(int(data[y][x + 1:rightmost + 1]))
 
-                    # TODO: Check above and below (between x-1 - x+1) for more numbers
+                    for i in range(-1, 2):
+                        if (y - 1 >= 0 and x + i < len(data[y - 1]) and x + i >= 0):
+                            if (data[y - 1][x + i].isdigit()):
+                                leftmost = x + i
+                                rightmost = x + i
+
+                                while (leftmost - 1 >= 0 and data[y - 1][leftmost - 1].isdigit()):
+                                    leftmost -= 1
+                                while (rightmost + 1 < len(data[y - 1]) and data[y - 1][rightmost + 1].isdigit()):
+                                    rightmost += 1
+                                if (num_list and num_list[-1] == int(data[y - 1][leftmost:rightmost + 1])):
+                                    continue
+                                num_list.append(int(data[y - 1][leftmost:rightmost + 1]))
+                                
+                    for i in range(-1, 2):
+                        if (y + 1 < len(data[y + 1]) and x + i < len(data[y + 1]) and x + i >= 0):
+                            if (data[y + 1][x + i].isdigit()):
+                                leftmost = x + i
+                                rightmost = x + i
+
+                                while (leftmost - 1 >= 0 and data[y + 1][leftmost - 1].isdigit()):
+                                    leftmost -= 1
+                                while (rightmost + 1 < len(data[y + 1]) and data[y + 1][rightmost + 1].isdigit()):
+                                    rightmost += 1
+                                if (num_list and num_list[-1] == int(data[y + 1][leftmost:rightmost + 1])):
+                                    continue
+                                num_list.append(int(data[y + 1][leftmost:rightmost + 1]))
 
                     if (len(num_list) == 2):
                         running_total += num_list[0] * num_list[1]
                         continue
+                    
         print(running_total)
     answer_2()
